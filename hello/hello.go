1e467a56f2ab4f229b078059414b26bab6856031
Chris Lemler <clemler@pm.me>
2019-08-24 08:28:23 -0700

    A few comments

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,11 @@
+// Simple Hello application used to demonstrate function calls, and parameters.
 package main
 
 import (
 	"fmt"
 )
 
+// Returns a greeting string
 func greeting() string {
 	return "Hello World"
 }
