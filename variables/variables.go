11333b7c9419852dcfcdf126f2e556591c9aea5b
Chris Lemler <clemler@pm.me>
2019-08-25 12:18:40 -0700

    Finished test cases and added an example for type conversion

--- variables/variables.go
+++ variables/variables.go
@@ -5,3 +5,8 @@ package variables
 func Add(x, y int) int {
 	return x + y
 }
+
+// AddConv - adds two 'ints' and returns their sum as an 'int32'
+func AddConv(x, y int) int32 {
+	return int32(x+y)
+}
--- variables/variables_test.go
+++ variables/variables_test.go
@@ -12,9 +12,24 @@ func TestAddr(t *testing.T) {
 	}
 }
 
+func TestConvInt32(t *testing.T) {
+	want := "int32"
+	got := fmt.Sprintf("%T", AddConv(16, 21))
+	if want != got {
+		t.Errorf("want % q, got %q", want, got)
+	}
+}
+
 // Adds two integers and returns their sum
 func ExampleAdd() {
 	sum := Add(4, 7)
 	fmt.Println(sum)
 	// Output: 11
+}
+
+// Adds two integers (int) and returns their sum as an (int32)
+func ExampleAddConv() {
+	var sum int32 = AddConv(16, 30)
+	fmt.Println(sum)
+	// Output: 46
 }
