75745a5bc73472e057c4a359695b9befaa618bb0
Chris Lemler <clemler@pm.me>
2019-09-10 06:48:25 -0700

    Added Withdraw function and tests

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -18,10 +18,24 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("Withdrawl", func(t *testing.T) {
+	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		// The balance should not change if the requested withdraw
+		// amount exceeds the wallets balance
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
 	})
 
 }
